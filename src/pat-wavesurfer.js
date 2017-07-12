3ce65fa1a586c42423e90506d3333a915f382ce8
Peter Holzer <peter.holzer@agitator.com>
2017-07-12 10:04:41 +0200

    added demo audio

--- src/pat-wavesurfer.js
+++ src/pat-wavesurfer.js
@@ -42,7 +42,8 @@
             var wavesurfer = WaveSurfer.create({
                 container: '#waveform'
             });
-
+            wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+            wavesurfer.playPause()
 
 
             log.debug('pattern initialized');
